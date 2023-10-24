e1eb8740636b34ce9137771c55cbf2399e11ce1c
alexk49 <alexcode9@mailbox.org>
2023-10-24 08:52:15 +0100

    added basic logic to handle log file not existing, and updated to rotating file handler for logs

--- config_dict.py
+++ config_dict.py
@@ -1,12 +1,24 @@
-from logging.config import dictConfig
+"""Config dict for log files"""
+from os.path import exists, join
+from os import mkdir
+
+LOGS_DIR = "logs"
+LOGS_FILE = "booksanon-logs.txt"
+LOGS_FILE_PATH = join(LOGS_DIR, LOGS_FILE)
+
+if exists(LOGS_DIR) is False:
+    mkdir(LOGS_DIR)
+
+if not (LOGS_FILE_PATH):
+    open(LOGS_FILE_PATH, "w").close()  # create empty log file
 
 # configire logs
 config_dict = {
     "version": 1,
     "formatters": {
         "default": {
-            "format": "[%(asctime)s] [%(levelname)s | %(module)s] %(message)s",
-            "datefmt": "%B %d, %Y %H:%M:%S %Z",
+            "format": "%(asctime)s - %(levelname)s - %(message)s",
+            "datefmt": "%B %d, %Y %H:%M:%S",
         },
     },
     "handlers": {
@@ -15,9 +27,12 @@ config_dict = {
             "formatter": "default",
         },
         "file": {
-            "class": "logging.FileHandler",
-            "filename": "booksanon.log",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": LOGS_FILE_PATH,
             "formatter": "default",
+            "mode": "a",
+            "maxBytes": 100000,
+            "backupCount": 2,
         },
     },
     "root": {"level": "DEBUG", "handlers": ["console", "file"]},
